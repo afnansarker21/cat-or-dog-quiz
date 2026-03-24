82fac1c218f8059ecaaaeef4d1dbbc29e01bc707
afnansarker21 <afnan.sarker@student.uts.edu.au>
2026-03-24 21:46:06 +1100

    update

---
+++ script.js
@@ -0,0 +1,192 @@
+const quizData = [
+    {
+    question: "What do you enjoy doing in your spare time?",
+    options: [
+        { text: "Going on hikes", animal: "Dog"},
+        { text: "Crochetting/Knitting", animal: "Cat"},
+        { text: "Napping", animal: "Cat"},
+        { text: "Playing sport", animal: "Dog"},
+        
+    ]
+
+    },
+
+    {
+        question: "How do you best describe yourself",
+        options: [
+            { text: "Introverted", animal: "Cat"},
+            { text: "Extroverted", animal: "Dog"},
+            { text: "Ambivert", animal: "Cat"},
+            
+        ]
+    
+        },
+
+
+    {
+        question: "What was your favourite subject at school?",
+        options: [
+            { text: "English", animal: "Cat"},
+            { text: "PE (Physical Education)", animal: "Dog"},
+            { text: "Art", animal: "Cat"},
+            { text: "Mathematics", animal: "Cat"},
+            { text: "Science", animal: "Dog"},
+            
+        ]
+    
+        },
+    {
+        question: "Do you like frequent parties e.g clubbing?",
+        options: [
+            { text: "No, I want to make time for others too.", animal: "Dog"},
+            { text: "Yes, I don't mind here and there", animal: "Cat"},
+            { text: "No, I dislike parties", animal: "Cat"},
+                
+        ]
+        
+        },
+
+    {
+        question: "What is your favourite food?",
+        options: [
+            { text: "Cupcakes", animal: "Cat"},
+            { text: "Pizza", animal: "Dog"},
+            { text: "Brownies", animal: "Cat"},
+            { text: "Tacos", animal: "Dog"},
+                    
+            ]
+            
+            },
+    {
+        question: "Right now, do you have a large backyard or enough space at home/neighbourhoods?",
+        options: [
+            { text: "Yes", animal: "Dog"},
+            { text: "No", animal: "Cat"},
+                            
+        ]
+                    
+         },
+
+    {
+        question: "Do you work long hours?",
+        options: [
+            { text: "Yes", animal: "Cat"},
+            { text: "No", animal: "Dog"},
+                                
+        ]
+                        
+        },
+
+    {
+        question: "What is your love language?",
+        options: [
+        { text: "Words of Affirmation", animal: "Cat"},
+        { text: "Quality Time", animal: "Dog"},
+        { text: "Physical Touch", animal: "Dog"},
+        { text: "Acts of Service", animal: "Cat"},
+        { text: "Receiving Gifts", animal: "Dog"},
+                                    
+        ]
+                            
+        },
+
+    {
+        question: "Do you consider yourself family oriented?",
+        options: [
+        { text: "Yes", animal: "Dog"},
+        { text: "Sometimes", animal: "Cat"},
+        { text: "No", animal: "Cat"},
+        
+                                        
+        ]
+                                
+     },
+
+
+     {
+        question: "What do your friends best describe you as?",
+        options: [
+        { text: "Loyal", animal: "Dog"},
+        { text: "Trustworthy", animal: "Cat"},
+        { text: "Reliable", animal: "Cat"},
+        { text: "Humourous", animal: "Dog"},
+        
+                                        
+        ]
+                                
+     },
+
+     {
+        question: "Your favourite drink?",
+        options: [
+        { text: "Matcha", animal: "Cat"},
+        { text: "Coffee", animal: "Cat"},
+        { text: "Coca Cola", animal: "Dog"},
+        { text: "Beer", animal: "Dog"},
+        
+                                        
+        ]
+                                
+     },
+
+     {
+        question: "Favourite genre of music?",
+        options: [
+        { text: "I don't listen to music", animal: "Cat"},
+        { text: "Jazz", animal: "Cat"},
+        { text: "Pop", animal: "Dog"},
+        { text: "Hip-hop", animal: "Dog"},
+        
+                                        
+        ]
+                                
+     },
+
+     {
+        question: "Where do you see yourself living in the next 5-10 years?",
+        options: [
+        { text: "Country town", animal: "Dog"},
+        { text: "Eco-Focused or Mountain Towns", animal: "Dog"},
+        { text: "Regional cities", animal: "Cats"},
+        { text: "City life", animal: "Dog"},
+        { text: "Normal suburban place", animal: "Cat"},
+        
+                                        
+        ]
+                                
+     },
+
+
+    {
+       question: "What phone do you own?",
+       options: [
+       { text: "Apple", animal: "Dog"},
+       { text: "Android", animal: "Cat"},
+       
+                                       
+       ]
+                               
+    },
+
+    {
+        question: "Are you a...",
+        options: [
+        { text: "Night-time person", animal: "Cat"},
+        { text: "Day-time person", animal: "Dog"},
+        
+                                        
+        ]
+                                
+     },
+
+
+    
+
+    
+    
+]
+
+        
+
+
+
